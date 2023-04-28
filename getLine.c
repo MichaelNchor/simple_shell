746ffc00d1c5dcec5cf511cdacc20d759a21fdfc
Nchor Michael <Manchor127@gmail.com>
2023-04-28 08:10:06 +0000

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
+ * buffers chained commands
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
@@ -45,7 +45,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 }
 
 /**
- * get_input - gets a line minus the newline
+ * gets a line minus the newline
  * @info: parameter struct
  *
  * Return: bytes read
@@ -90,7 +90,7 @@ ssize_t get_input(info_t *info)
 }
 
 /**
- * read_buf - reads a buffer
+ * reads a buffer
  * @info: parameter struct
  * @buf: buffer
  * @i: size
@@ -110,7 +110,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 }
 
 /**
- * _getline - gets the next line of input from STDIN
+ * gets the next line of input from STDIN
  * @info: parameter struct
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
@@ -157,7 +157,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 }
 
 /**
- * sigintHandler - blocks ctrl-C
+ * blocks ctrl-C
  * @sig_num: the signal number
  *
  * Return: void
