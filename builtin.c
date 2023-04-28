2059a640dde1358bfdcfbf598c76a202af506dc0
Nchor Michael <Manchor127@gmail.com>
2023-04-28 08:03:54 +0000

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
+ * exits the shell
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  
@@ -29,7 +29,7 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
+ * changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
@@ -79,7 +79,7 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
+ * changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
